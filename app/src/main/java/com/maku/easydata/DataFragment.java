6fd88c6b15c04d83678e934612353167a1cafb24
MAZAKPE <makpalyy@gmail.com>
2019-08-12 06:21:42 +0200

    added the local server module

--- app/src/main/java/com/maku/easydata/DataFragment.java
+++ app/src/main/java/com/maku/easydata/DataFragment.java
@@ -1,24 +1,16 @@
 package com.maku.easydata;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.maku.easydata.adapter.DataRecyclerviewAdapter;
-import com.maku.easydata.models.DataModel;
-import com.maku.easydata.utils.DataPrices;
 
 import java.util.ArrayList;
 
@@ -29,15 +21,6 @@ public class DataFragment extends Fragment {
 
     //constants
 
-    //widgets
-    private RecyclerView mRecyclerView;
-    private SwipeRefreshLayout mSwipeRefreshLayout;
-
-    //vars
-    private ArrayList<DataModel> mData = new ArrayList<>();
-    private LinearLayoutManager mLinearLayoutManager;
-    private DataRecyclerviewAdapter mRecyclerViewAdapter;
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,38 +28,7 @@ public class DataFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_data, container, false);
 
-//        recyclerview
-        mRecyclerView = view.findViewById(R.id.recycler_view_data);
-
-        findCategory();
-
         return view;
     }
-
-    private void findCategory() {
-        DataPrices dataPrices = new DataPrices();
-        if (mData != null) {
-            mData.clear();
-        }
-        for (DataModel category : dataPrices.DATA) {
-            mData.add(category);
-        }
-        if (mRecyclerViewAdapter == null) {
-            initRecyclerView();
-        }
-    }
-
-    private void initRecyclerView(){
-        Log.d(TAG, "initRecyclerView: init recyclerview.");
-        mLinearLayoutManager = new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL,false);
-        mRecyclerView.setLayoutManager(mLinearLayoutManager);
-        mRecyclerViewAdapter = new DataRecyclerviewAdapter(mData, getActivity());
-        mRecyclerView.setAdapter(mRecyclerViewAdapter);
-    }
-
-    public void scrollToTop(){
-        mRecyclerView.smoothScrollToPosition(0);
-    }
-
 
 }
